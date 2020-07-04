07715a3ea9fbd1c2b5e11771674fb91d68c89beb
Varun Sharma <coding.with.binaries@gmail.com>
2020-07-04 22:06:16 +0530

    Added Header and Custom Input

--- little-groot-client/src/App.vue
+++ little-groot-client/src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="little-groot">
+    <GrootHeader />
     <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import GrootHeader from '@/components/groot-header/GrootHeader.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    GrootHeader
+  }
+};
+</script>
+
 <style lang="scss">
-#app {
+body {
+  margin: 0;
+}
+#little-groot {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ little-groot-client/src/components/groot-header/GrootHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="groot-header">
+    <div class="groot-categories"></div>
+    <span class="groot-title">little-groot</span>
+    <div class="groot-user-options"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import GrootInput from '../groot-input-components/groot-input/GrootInput.vue';
+
+@Component({ components: { GrootInput } })
+export default class GrootHeader extends Vue {
+  private value!: string;
+}
+</script>
+
+<style scoped lang="scss">
+#groot-header {
+  height: 4rem;
+
+  display: grid;
+  grid-template-columns: 1fr 10rem 1fr;
+  align-items: center;
+
+  background-color: #42b983;
+  color: white;
+
+  .groot-categories {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .groot-title {
+    font-size: x-large;
+    cursor: pointer;
+  }
+}
+</style>
